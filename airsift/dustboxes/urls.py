08cef0daf587846ad48fd054a4910a5ffdc58d7a
Jan Baykara <janbaykara@pm.me>
2020-11-26 12:10:42 +0000

    Add dustboxes menu item

--- airsift/dustboxes/urls.py
+++ airsift/dustboxes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 from django.views.generic.base import TemplateView
 from . import views, data
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dustboxes/analysis.html')),
+    path('', lambda request: redirect('/dustboxes')),
     path('analysis', TemplateView.as_view(template_name='dustboxes/analysis.html')),
     path('dustboxes', views.dustboxes),
     re_path(r'^citizensense/(.*)', data.citizensense_data)
