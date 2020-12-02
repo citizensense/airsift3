40a65c5dd90651325ded16def70990962e26b2a9
Jan Baykara <janbaykara@pm.me>
2020-12-02 10:40:08 +0000

    Add a checkbox for terms and conditions

--- airsift/users/forms.py
+++ airsift/users/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
 from django.forms import fields
 from django.core.exceptions import ValidationError
+from django.forms import forms
 from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
@@ -34,18 +35,26 @@ class UserCreationForm(admin_forms.UserCreationForm):
 
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.utils.safestring import mark_safe
+
 editors_group = Group.objects.get(name='Editors')
 
 class CustomSignupForm(SignupForm):
     name = fields.CharField(max_length=100, label='Real Name')
+    terms = fields.BooleanField(label=mark_safe('I agree to the <a href="https://citizensense.net/about/terms/" class="border-b border-brand">Terms and Conditions</a>'), required=True)
 
     def __init__(self, *args, **kwargs):
         super(CustomSignupForm, self).__init__(*args, **kwargs)
         self.fields.pop('password2')
 
+    def clean_terms(self):
+        if not self:
+            return forms.ValidationError("You must accept the terms and conditions")
+
     def save(self, request):
         user = super(CustomSignupForm, self).save(request)
         user.password2 = user.password
+        user.checkbox = None
         user.save()
         # Allow the user access to the Wagtail CMS
         editors_group.user_set.add(user)
