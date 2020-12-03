34abe130221956b16572288d9f49ac7f88c0b64c
Jan Baykara <janbaykara@pm.me>
2020-12-03 17:03:11 +0000

    Example of how to style wagtail with basic colours

--- airsift/users/wagtail_hooks.py
+++ airsift/users/wagtail_hooks.py
@@ -19,3 +19,24 @@ def filter_images_by_user(images, request):
     images = images.filter(uploaded_by_user=request.user)
 
     return images
+
+from django.conf.urls.static import static
+from django.utils.html import format_html
+
+from wagtail.core import hooks
+
+@hooks.register("insert_global_admin_css", order=100)
+def global_admin_css():
+    """Add /static/css/custom.css to the admin."""
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        "/static/wagtail/main.css"
+    )
+
+@hooks.register("insert_global_admin_js", order=100)
+def global_admin_js():
+    """Add /static/css/custom.js to the admin."""
+    return format_html(
+        '<script src="{}"></script>',
+        "/static/wagtail/main.js"
+    )
