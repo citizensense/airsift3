e1cc3129697552ebc24b75f58105b6f0872612e4
Jan Baykara <janbaykara@pm.me>
2020-12-15 10:20:30 +0000

    Made user signup more resilient

--- airsift/users/models.py
+++ airsift/users/models.py
@@ -40,8 +40,10 @@ class User(AbstractUser):
 
     def contributor_page_permissions_page(self):
         page_permission = self.contributor_page_permissions()
+        if not page_permission:
+            self.create_user_group_and_pages()
+            page_permission = self.contributor_page_permissions()
         page = page_permission.page
-        # TODO: Create if doesn't exist?
         return page
 
     def is_staff_editor(self):
@@ -61,8 +63,6 @@ class User(AbstractUser):
             return self.contributor_page_permissions_page()
 
     def has_contributor_group(self):
-        print(self.contributor_group_name())
-        print(self.contributor_group())
         return self.contributor_group() is not None
 
     def get_user_action_urls(self):
@@ -70,17 +70,100 @@ class User(AbstractUser):
 
         if page is None:
             # Trigger login to figure out what to do
-            return {
+            urls = {
                 "view_root_page": f'/cms/pages/',
                 "create_observation": f'/user_action_redirect/create_observation/',
                 "create_datastory": f'/user_action_redirect/create_datastory/',
             }
+            return urls
         else:
-            return {
+            urls = {
                 "view_root_page": f'/cms/pages/{page.id}/',
                 "create_observation": f'/cms/pages/add/observations/observation/{page.id}/',
                 "create_datastory": f'/cms/pages/add/datastories/datastory/{page.id}/',
             }
+            return urls
+
+    def get_user_contributions_root(user):
+        title = 'User contributions'
+        slug = 'posts'
+        #
+        root = HomePage.get_active()
+        try:
+            page = Page.objects.get(slug=slug)
+            return page
+        except:
+            page = Page(title=title, slug=slug)
+            root.add_child(instance=page)
+            return Page.objects.get(slug=slug)
+
+    def create_user_group_and_pages(user):
+        """
+        When a new user signs up create a unique group and page for them.
+        Assign it the appropriate permission for admin, page and collection access.
+        """
+
+        # Create a group object that matches their username
+        new_group, created = Group.objects.get_or_create(
+            name=user.scoped_contributions_group_name()
+        )
+
+        # Add the new group to the database
+        user.groups.add(new_group)
+
+        # Create new permission to access the wagtail admin
+        access_admin = Permission.objects.get(
+            content_type__app_label='wagtailadmin',
+            codename='access_admin'
+        )
+
+        # Add the permission to the group
+        new_group.permissions.add(access_admin)
+
+        # Now start creating page access
+        # Create unique UserIndexPage for the user
+        person_index_page = UserIndexPage(
+            title=f"{user.name}'s contributions",
+            slug=user.username
+        )
+
+        # Add UserIndexPage for user
+        contributions_page = user.get_user_contributions_root()
+        contributions_page.add_child(instance=person_index_page)
+
+        # Save new page as first revision
+        person_index_page.save_revision()
+
+        # Create new add GroupPagePermission
+        for permission in [
+            # Allow page creation
+            PAGE_PERMISSION_TYPES[0][0],
+            # Allow edits
+            PAGE_PERMISSION_TYPES[1][0],
+            # Allow users to publish their pages straight away
+            PAGE_PERMISSION_TYPES[2][0],
+        ]:
+            GroupPagePermission.objects.create(
+                group=new_group,
+                page=person_index_page,
+                permission_type=permission
+            )
+
+        # Create a collection that this user can put images in
+        root_collection = Collection.get_first_root_node()
+        image_collection = Collection(name=f"{user.name}'s images")
+        root_collection.add_child(instance=image_collection)
+
+        # Create new GroupCollectionPermission for Profile Images collection
+        GroupCollectionPermission.objects.create(
+            group=new_group,
+            collection=image_collection,
+            permission=Permission.objects.get(
+                content_type__app_label='wagtailimages',
+                codename='add_image'
+            )
+        )
+
 
 class UserIndexPage(Page):
     '''
@@ -93,72 +176,3 @@ class UserIndexPage(Page):
         'datastories.DataStory',
     ]
     pass
-
-@receiver(user_signed_up)
-def create_user_group_and_pages(sender, **kwargs):
-    """
-    When a new user signs up create a unique group and page for them.
-    Assign it the appropriate permission for admin, page and collection access.
-    """
-
-    # Grab the new user
-    user = kwargs['user']
-
-    # Create a group object that matches their username
-    new_group, created = Group.objects.get_or_create(name=user.scoped_contributions_group_name())
-
-    # Add the new group to the database
-    user.groups.add(new_group)
-
-    # Create new permission to access the wagtail admin
-    access_admin = Permission.objects.get(
-        content_type__app_label='wagtailadmin',
-        codename='access_admin'
-    )
-
-    # Add the permission to the group
-    new_group.permissions.add(access_admin)
-
-    # Now start creating page access
-    # Create unique UserIndexPage for the user
-    person_index_page = UserIndexPage(
-        title=f"{user.name}'s contributions",
-        slug=user.username
-    )
-
-    # Add UserIndexPage for user
-    contributions = Page.objects.get(slug='posts')
-    contributions.add_child(instance=person_index_page)
-
-    # Save new page as first revision
-    person_index_page.save_revision()
-
-    # Create new add GroupPagePermission
-    for permission in [
-        # Allow page creation
-        PAGE_PERMISSION_TYPES[0],
-        # Allow edits
-        PAGE_PERMISSION_TYPES[1],
-        # Allow users to publish their pages straight away
-        PAGE_PERMISSION_TYPES[2],
-    ]:
-        GroupPagePermission.objects.create(
-            group=new_group,
-            page=person_index_page,
-            permission_type=permission
-        )
-
-    # Create a collection that this user can put images in
-    root_collection = Collection.get_first_root_node()
-    image_collection = Collection(name=f"{user.name}'s images")
-    root_collection.add_child(instance=image_collection)
-
-    # Create new GroupCollectionPermission for Profile Images collection
-    GroupCollectionPermission.objects.create(
-        group=new_group,
-        collection=image_collection,
-        permission=Permission.objects.get(
-            content_type__app_label='wagtailimages',
-            codename='add_image'
-        )
-    )
