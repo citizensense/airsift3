434aebce3eff441b536d0d60a3aa0df44275ab31
Jan Baykara <janbaykara@pm.me>
2020-12-16 11:21:59 +0000

    Link footer image to citizensense.net
    Footer

--- airsift/static/map/scaffolding.tsx
+++ airsift/static/map/scaffolding.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className='px-4 mt-4 pb-3 uppercase font-cousine text-XS'>
-      <img src={'/static/images/citizenSenseLogo.png'} className='mb-3 w-1/2' />
+      <a href='https://citizensense.net' target='_blank'>
+        <img src={'/static/images/citizenSenseLogo.png'} className='mb-3 w-1/2' />
+      </a>
       <a href='https://citizensense.net/about/contact/'>Contact</a>
       <a className='ml-3' href='https://citizensense.net/about/terms/'>Terms &amp; Conditions</a>
     </div>
