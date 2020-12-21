faa82b31352a5d731132b1f6f117bc93752172db
Jan Baykara <janbaykara@pm.me>
2020-12-21 16:30:00 +0000

    Disable 'broken INTERNAL link' emails being sent to admins

--- config/settings/base.py
+++ config/settings/base.py
@@ -166,7 +166,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
